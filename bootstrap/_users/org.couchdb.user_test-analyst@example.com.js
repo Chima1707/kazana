ab3cb636ca12f993dc7173bfdd5fecd098ca2978
Gregor Martynus <gregor@martynus.net>
2015-08-04 23:39:31 -0400

    style: indentation

--- bootstrap/_users/org.couchdb.user_test-analyst@example.com.js
+++ bootstrap/_users/org.couchdb.user_test-analyst@example.com.js
@@ -1,14 +1,14 @@
 var password = require('kazana-config').pouchdbHttpAdminPass;
 
 module.exports = {
-   _id: 'org.couchdb.user:kazana-analyst@example.com',
-   type: 'user',
-   name: 'kazana-analyst@example.com',
-   roles: [
-       'kazana-id:testanalyst',
-       'kazana-analyst'
-   ],
-   password: password,
-   createdAt: '2015-04-15T00:00:00.000Z',
-   fullname: 'Test Analyst'
+  _id: 'org.couchdb.user:kazana-analyst@example.com',
+  type: 'user',
+  name: 'kazana-analyst@example.com',
+  roles: [
+    'kazana-id:testanalyst',
+    'kazana-analyst'
+  ],
+  password: password,
+  createdAt: '2015-04-15T00:00:00.000Z',
+  fullname: 'Test Analyst'
 };
--- bootstrap/_users/org.couchdb.user_test-data-provider@example.com.js
+++ bootstrap/_users/org.couchdb.user_test-data-provider@example.com.js
@@ -1,14 +1,14 @@
 var password = require('kazana-config').pouchdbHttpAdminPass;
 
 module.exports = {
-   _id: 'org.couchdb.user:kazana-data-provider@example.com',
-   type: 'user',
-   name: 'kazana-data-provider@example.com',
-   roles: [
-       'kazana-id:testdataprovider',
-       'kazana-data-provider'
-   ],
-   password: password,
-   createdAt: '2015-04-15T00:00:00.000Z',
-   fullname: 'Test Data Provider'
+  _id: 'org.couchdb.user:kazana-data-provider@example.com',
+  type: 'user',
+  name: 'kazana-data-provider@example.com',
+  roles: [
+    'kazana-id:testdataprovider',
+    'kazana-data-provider'
+  ],
+  password: password,
+  createdAt: '2015-04-15T00:00:00.000Z',
+  fullname: 'Test Data Provider'
 };
